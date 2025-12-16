cce5f8735c9a17a3ce0ba9b2be277f89abb4a6ca
zonik811 <oscarcubillos811@gmail.com>
2025-12-15 19:15:19 -0500

    Fix suspense boundary5

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, ShoppingBag, Settings, LogOut, Store, Tag, Palette } from "lucide-react";
 import Link from "next/link";
